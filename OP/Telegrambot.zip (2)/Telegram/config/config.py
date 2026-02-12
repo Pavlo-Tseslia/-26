31d0bf7cf5076dc566531eaf69b62f58ee05fe34
Pavlo-Tseslia <pashacesla@gmail.com>
2026-02-12 22:04:58 +0200

    Delete OP directory

--- OP/Telegrambot.zip (2)/Telegram/config/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-TOKEN = "8096671270:AAE9sgF1Ia3vEiSCqWy4x48pIBsYF9YEwJs"
-
-load_dotenv()
-
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
--- OP/Telegrambot/config/init.py
+++
@@ -1,6 +0,0 @@
-d = float(input("Дільник: "))
-
-if d == 0:
-    print("На нуль ділити не можна")
-else:
-    print(10 / d)
